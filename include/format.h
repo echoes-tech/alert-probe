134591c3b508ee9baee9db2149e22433b2392b9b
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2012-06-06 22:29:42 +0200

    Creation structure for Format Module

---
+++ include/format.h
@@ -0,0 +1,16 @@
+/* 
+ * Header of Format Module
+ * @author ECHOES Technologies (FPO)
+ * @date 24/04/2012
+ */
+
+
+#ifndef FORMAT_H
+#define	FORMAT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#endif	/* FORMAT_H */
+
---
+++ src/format.c
@@ -0,0 +1,14 @@
+/* 
+ * Format Module
+ * @author ECHOES Technologies (FPO)
+ * @date 24/04/2012
+ */
+
+#include "format.h"
+
+int format()
+{
+    
+    return (EXIT_SUCCESS);
+}
+
