7b9e05742002f54150e9a5f03d465dd69d1511be
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2012-12-21 18:03:00 +0100

    Install script: URL encoding for login and password

--- install.sh
+++ install.sh
@@ -188,15 +188,18 @@ echo "Note: login is $LOGIN"
 read -s -p "Password: " PASSWORD
 echo ""
 
+# URL Encoding
+LOGIN_ENC=$(echo "$LOGIN" | sed -e 's/%/%25/g' -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\$/%24/g' -e 's/\&/%26/g' -e "s/'/%27/g" -e 's/(/%28/g' -e 's/)/%29/g' -e 's/\*/%2A/g' -e 's/+/%2B/g' -e 's/,/%2C/g' -e 's/-/%2D/g' -e 's/\./%2E/g' -e 's/\//%2F/g' -e 's/:/%3A/g' -e 's/;/%3B/g' -e 's/</%3C/g' -e 's/=/%3D/g' -e 's/>/%3E/g' -e 's/?/%3F/g' -e 's/@/%40/g' -e 's/\[/%5B/g' -e 's/\\/%5C/g' -e 's/\]/%5D/g' -e 's/\^/%5E/g' -e 's/_/%5F/g' -e 's/`/%60/g' -e 's/{/%7B/g' -e 's/|/%7C/g' -e 's/}/%7D/g' -e 's/~/%7E/g')
+PASSWORD_ENC=$(echo "$PASSWORD" | sed -e 's/%/%25/g' -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\$/%24/g' -e 's/\&/%26/g' -e "s/'/%27/g" -e 's/(/%28/g' -e 's/)/%29/g' -e 's/\*/%2A/g' -e 's/+/%2B/g' -e 's/,/%2C/g' -e 's/-/%2D/g' -e 's/\./%2E/g' -e 's/\//%2F/g' -e 's/:/%3A/g' -e 's/;/%3B/g' -e 's/</%3C/g' -e 's/=/%3D/g' -e 's/>/%3E/g' -e 's/?/%3F/g' -e 's/@/%40/g' -e 's/\[/%5B/g' -e 's/\\/%5C/g' -e 's/\]/%5D/g' -e 's/\^/%5E/g' -e 's/_/%5F/g' -e 's/`/%60/g' -e 's/{/%7B/g' -e 's/|/%7C/g' -e 's/}/%7D/g' -e 's/~/%7E/g')
 
 TMP_DIR=$(mktemp -d -t ea-probe-install-XXXXXXXXXX)
 cd $TMP_DIR
 
-post "$API_URI/asset/$ASSET_ID/probe?login=$LOGIN&password=$PASSWORD" "{\"distrib\":{\"name\":\"$HOST_DISTRIB\",\"release\":\"$DISTRIB_RELEASE\"},\"arch\":\"$HOST_ARCH\"}" probe_request_res
+post "$API_URI/asset/$ASSET_ID/probe?login=$LOGIN_ENC&password=$PASSWORD_ENC" "{\"distrib\":{\"name\":\"$HOST_DISTRIB\",\"release\":\"$DISTRIB_RELEASE\"},\"arch\":\"$HOST_ARCH\"}" probe_request_res
 
 parse_probe_request_res
 
-get "$API_URI/probe/$PROBE_ID?login=$LOGIN&password=$PASSWORD" $PKG
+get "$API_URI/probe/$PROBE_ID?login=$LOGIN_ENC&password=$PASSWORD_ENC" $PKG
 echo "ECHOES Alert Probe downloaded."
 
 probe_installation
@@ -205,7 +208,7 @@ echo "ECHOES Alert Probe installed."
 sed -i -e "s/\(probe_id=\).*/\1$PROBE_ID/" -e "s/\(token=\).*/\1$TOKEN/" $INSTALL_DIR/etc/probe.conf
 echo "ECHOES Alert Probe configured."
 
-get "$API_URI/asset/$ASSET_ID/plugin?login=$LOGIN&password=$PASSWORD" plugins_list_res
+get "$API_URI/asset/$ASSET_ID/plugin?login=$LOGIN_ENC&password=$PASSWORD_ENC" plugins_list_res
 
 mkdir plugins
 cd plugins
@@ -229,7 +232,7 @@ do
 		if [ $plugin_name ]
 		then
 			check_id=true
-			get "$API_URI/asset/$ASSET_ID/plugin/$plugin_id?login=$LOGIN&password=$PASSWORD" "$plugin_name"
+			get "$API_URI/asset/$ASSET_ID/plugin/$plugin_id?login=$LOGIN_ENC&password=$PASSWORD_ENC" "$plugin_name"
 			plugin_id=""
 			plugin_name=""
 		fi
