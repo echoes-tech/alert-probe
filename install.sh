35b4e96dc9a429281d1bf259669ee66a67815495
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2013-01-08 20:03:13 +0100

    BugFix install.sh : Anomalie #213 Anomalie #219

--- install.sh
+++ install.sh
@@ -185,12 +185,15 @@ check_host_arch
 
 echo "To download ECHOES Alert Probe, you have to enter your password."
 echo "Note: login is $LOGIN"
-read -s -p "Password: " PASSWORD
+echo -n "Password: "
+stty -echo; read PASSWORD; stty echo
 echo ""
 
 # URL Encoding
 LOGIN_ENC=$(echo "$LOGIN" | sed -e 's/%/%25/g' -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\$/%24/g' -e 's/\&/%26/g' -e "s/'/%27/g" -e 's/(/%28/g' -e 's/)/%29/g' -e 's/\*/%2A/g' -e 's/+/%2B/g' -e 's/,/%2C/g' -e 's/-/%2D/g' -e 's/\./%2E/g' -e 's/\//%2F/g' -e 's/:/%3A/g' -e 's/;/%3B/g' -e 's/</%3C/g' -e 's/=/%3D/g' -e 's/>/%3E/g' -e 's/?/%3F/g' -e 's/@/%40/g' -e 's/\[/%5B/g' -e 's/\\/%5C/g' -e 's/\]/%5D/g' -e 's/\^/%5E/g' -e 's/_/%5F/g' -e 's/`/%60/g' -e 's/{/%7B/g' -e 's/|/%7C/g' -e 's/}/%7D/g' -e 's/~/%7E/g')
 PASSWORD_ENC=$(echo "$PASSWORD" | sed -e 's/%/%25/g' -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\$/%24/g' -e 's/\&/%26/g' -e "s/'/%27/g" -e 's/(/%28/g' -e 's/)/%29/g' -e 's/\*/%2A/g' -e 's/+/%2B/g' -e 's/,/%2C/g' -e 's/-/%2D/g' -e 's/\./%2E/g' -e 's/\//%2F/g' -e 's/:/%3A/g' -e 's/;/%3B/g' -e 's/</%3C/g' -e 's/=/%3D/g' -e 's/>/%3E/g' -e 's/?/%3F/g' -e 's/@/%40/g' -e 's/\[/%5B/g' -e 's/\\/%5C/g' -e 's/\]/%5D/g' -e 's/\^/%5E/g' -e 's/_/%5F/g' -e 's/`/%60/g' -e 's/{/%7B/g' -e 's/|/%7C/g' -e 's/}/%7D/g' -e 's/~/%7E/g')
+
+unset LOGIN PASSWORD
 
 TMP_DIR=$(mktemp -d -t ea-probe-install-XXXXXXXXXX)
 cd $TMP_DIR
@@ -216,22 +219,22 @@ cd plugins
 plugin_id=""
 plugin_name=""
 
-check_id=true
+check_plugin_id=true
 
 while read line
 do
-	if [ $($check_id && echo "true" || echo "false") == "true" ]
+	if $check_plugin_id
 	then
 		plugin_id=$(echo $line | grep \"id\" | sed -e 's/ //g' | cut -d':' -f 2 | cut -d',' -f 1)
 	fi
 
 	if [ $plugin_id ]
 	then
-		check_id=false
+		check_plugin_id=false
 		plugin_name=$(echo $line | grep \"name\" | sed -e 's/ //g' | cut -d':' -f 2 | cut -d',' -f 1 | sed -e 's/\"//g')
 		if [ $plugin_name ]
 		then
-			check_id=true
+			check_plugin_id=true
 			get "$API_URI/asset/$ASSET_ID/plugin/$plugin_id?login=$LOGIN_ENC&password=$PASSWORD_ENC" "$plugin_name"
 			plugin_id=""
 			plugin_name=""
@@ -244,5 +247,7 @@ cp * $INSTALL_DIR/etc/plugins/
 
 cd "$CURRENT_DIR"
 rm -rf "$TMP_DIR"
+
+unset LOGIN_ENC PASSWORD_ENC
 
 service ea-probe start
