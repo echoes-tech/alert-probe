68134951b60073c98bae0ed02659811e70ee6e45
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2014-06-09 16:06:28 +0200

    BugFix RPM script

--- packages/var-rpm.sh
+++ packages/var-rpm.sh
@@ -1,41 +1,48 @@
-#!/bin/bash -x
+#!/bin/sh -x
 
-rpm_post="cd ${PACKAGE_TOP_DIR}
+rpm_post="cd /${PACKAGE_TOP_DIR}
 if [ -f lib.tar.gz ]
 then
-    tar xzf lib.tar.gz
-    rm -f lib.tar.gz
+  tar xzf lib.tar.gz
+  rm -f lib.tar.gz
 fi
 chkconfig --add $bin_name && chkconfig $bin_name on
 "
 
-rpm_postun='cd ${PACKAGE_TOP_DIR}
+rpm_postun="cd /${PACKAGE_TOP_DIR}
 if [ -d sbin ]
 then
-    rm -rf sbin
+  rm -rf sbin
 fi
-if [ `find /opt/echoes-alert/probe -type f | wc -l` -eq 0 ]
+if [ -d /opt/echoes-alert/probe ]
 then
+  if [ \`find /opt/echoes-alert/probe -type f | wc -l\` -eq 0 ]
+  then
     rm -rf /opt/echoes-alert/probe
-    if [ `find /opt/echoes-alert -type f | wc -l` -eq 0 ]
+    if [ -d /opt/echoes-alert ]
     then
+      if [ \`find /opt/echoes-alert -type f | wc -l\` -eq 0 ]
+      then
         rm -rf /opt/echoes-alert
+      fi
     fi
-fi'
+  fi
+fi
+"
 
 rpm_preun="service $bin_name stop
-cd ${PACKAGE_TOP_DIR}
+cd /${PACKAGE_TOP_DIR}
 if [ -d etc ]
 then
-    rm -rf etc
+  rm -rf etc
 fi
 if [ -d lib ]
 then
-    rm -rf lib
+  rm -rf lib
 fi
 if [ -d doc ]
 then
-    rm -rf doc
+  rm -rf doc
 fi
 chkconfig $bin_name off
 "
@@ -47,11 +54,12 @@ rpm_files="\"/${PACKAGE_TOP_DIR}/sbin/ea-probe\"
 \"/etc/sysconfig/ea-probe\"
 \"/${PACKAGE_TOP_DIR}/doc/AUTHORS\"
 \"/${PACKAGE_TOP_DIR}/doc/README\"
-\"/${PACKAGE_TOP_DIR}/doc/COPYRIGHT\""
+\"/${PACKAGE_TOP_DIR}/doc/COPYRIGHT\"
+"
 
 # Sysconfig
-createVarDir "rpm_sysconfig" "${NBTMPDIR}/etc/sysconfig/" 0755
+createVarDir 'rpm_sysconfig' "${NBTMPDIR}/etc/sysconfig/" 0755
 
-createVarFile "rpm_sysconfig" "packages/rpm/echoes-alert-probe.sysconfig" "${NBTMPDIR}/etc/sysconfig/ea-probe" 0755
+createVarFile 'rpm_sysconfig' 'packages/rpm/echoes-alert-probe.sysconfig' "${NBTMPDIR}/etc/sysconfig/ea-probe" 0755
 
-createVarFile "rpm_init" "packages/rpm/echoes-alert-probe.init" "${NBTMPDIR}/etc/init.d/ea-probe" 0755
+createVarFile 'rpm_init' 'packages/rpm/echoes-alert-probe.init' "${NBTMPDIR}/etc/init.d/ea-probe" 0755
